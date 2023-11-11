14822b818f37450b48639f4fd654338abdca6c58
alencarfbitalo <alencarfbitalo@hotmail.com>
2023-11-10 21:10:16 -0300

    mudar

---
+++ Aula12/scriptquadrado.js
@@ -0,0 +1,6 @@
+let ladoATexto = prompt("digite o lado A do quadrado")
+let ladoANumero = parseInt(ladoATexto)
+
+let ladoB = parseInt(prompt("Digite o lado B do quadrado"))
+
+alert(ladoAnumero + ladoB)
