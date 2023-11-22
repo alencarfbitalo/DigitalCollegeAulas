50d2a0cba0c3fde281920e95cbcaaa581574adc0
alencarfbitalo <alencarfbitalo@hotmail.com>
2023-11-22 18:10:55 -0300

    //comentario

--- Avaliacao/primeira.js
+++ Avaliacao/primeira.js
@@ -1,7 +1,7 @@
 // 1) Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C. 
 
 
-//Receberá os valores aplicados pelo usuario
+// Receberá os valores aplicados pelo usuario
 let a = parseFloat(prompt("Digite um valor para A:"))
 let b = parseFloat(prompt("Digite um valor para B:"))
 let c = parseFloat(prompt("Digite um valor para C:"))
