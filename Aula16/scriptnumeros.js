35cd3b16d2cc235f0ed8d1b917af3f4605a4a2a9
alencarfbitalo <alencarfbitalo@hotmail.com>
2023-12-08 20:43:04 -0300

    atualizar

--- Aula16/scriptnumeros.js
+++ Aula16/scriptnumeros.js
@@ -1,15 +1,16 @@
 function calcula() {
-    // Obter os valores dos inputs
+    // Usuario digitara os valores
     var n1 = parseFloat(document.getElementById('n1').value);
     var n2 = parseFloat(document.getElementById('n2').value);
 
     // Verifica os valores
+    //( && ) retorna verdadeiro se os dois operandos forem verdadeiros, caso contrário, retorna false
     if (!isNaN(n1) && !isNaN(n2)) {
 
         // Calcula a soma
         var soma = n1 + n2;
 
-        // Exibir o resultado na página
-        document.getElementById('resultado').innerText = 'Resultado: ' + soma;
+        // Mostra o resultado
+        document.getElementById('resultado').innerText = 'Resultado = ' + soma;
     } 
 }
